c71633055a00bb8ce9dc15520758c35cf123383f
Cangyuan Li <everest229@gmail.com>
2023-08-14 10:48:45 -0400

    adjust imports

--- src/pyethnicity/__init__.py
+++ src/pyethnicity/__init__.py
@@ -1,3 +1,3 @@
 __version__ = "0.0.1"
-from .bayesian_models import BayesianModel
-from .keras_models import predict_race_ensemble, predict_race_fl, predict_race_flg
+from ._bayesian_models import bifsg, bisg
+from ._ml_models import predict_race, predict_race_fl, predict_race_flg
