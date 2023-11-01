0aa27acd79d20fb9cbd3ccd2a9e0896430e925f4
Cangyuan Li <everest229@gmail.com>
2023-11-01 13:09:59 -0400

    version 0.0.11

--- src/pyethnicity/__init__.py
+++ src/pyethnicity/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.10"
+__version__ = "0.0.11"
 from ._bayesian_models import bifsg, bisg, predict_sex_ssa
 from ._ml_models import predict_race, predict_race_fl, predict_race_flg, predict_sex_f
