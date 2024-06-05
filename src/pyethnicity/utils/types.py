02b30cfd9c11d35213273b8386594f189b2ed450
Cangyuan Li <everest229@gmail.com>
2024-06-05 00:30:33 -0400

    use polars ArrayLike instead of our own

--- src/pyethnicity/utils/types.py
+++ src/pyethnicity/utils/types.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
-from collections.abc import Sequence
 from typing import Literal, Union
 
-import numpy as np
-import pandas as pd
-import polars as pl
+from polars.series.series import ArrayLike
 
-ArrayLike = Union[Sequence, pd.Series, pl.Series, np.ndarray]
 Name = Union[str, ArrayLike]
 Geography = Union[int, str, ArrayLike]
 GeoType = Literal["zcta", "tract", "block_group"]
