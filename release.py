8d207e240fc4decd63d5359f7e12868e3ca55b75
Cangyuan Li <everest229@gmail.com>
2024-06-05 00:35:55 -0400

    release script

---
+++ release.py
@@ -0,0 +1,23 @@
+import subprocess
+import time
+
+
+def main():
+    with open("src/pyethnicity/__init__.py") as f:
+        version = f.readlines()[0].split("=")[-1].strip().strip('"')
+
+    print("committing version changes")
+    subprocess.run(["git", "commit", "-am", version], check=True)
+
+    print("pushing to remote")
+    subprocess.run(["git", "push", "origin"], check=True)
+    time.sleep(2)
+
+    print("creating release")
+    subprocess.run(
+        ["gh", "release", "create", f"v{version}", "--generate-notes"], check=True
+    )
+
+
+if __name__ == "__main__":
+    main()
