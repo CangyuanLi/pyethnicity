415d70a2e3650ffa41a6c6bc44bba8a840210a10
Cangyuan Li <everest229@gmail.com>
2024-06-06 00:42:23 -0400

    0.0.25

--- src/pyethnicity/__init__.py
+++ src/pyethnicity/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.24"
+__version__ = "0.0.25"
 from ._bayesian_models import bifsg, bifsg6, bisg, bisg6, predict_sex_ssa
 from ._ml_models import predict_race, predict_race_fl, predict_race_flg, predict_sex_f
